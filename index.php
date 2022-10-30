abae4d30adda2228befd44a194284200cd040818
gumaath <guuhsykees@gmail.com>
2022-10-29 23:18:11 -0300

    change path

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="./src/css/bootstrap.css" rel="stylesheet">
   </head>
   <body>
     <header class="p-3 mb-3 border-bottom text-bg-dark">
@@ -28,6 +28,6 @@
         </div>
       </div>
     </header>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="./src/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
