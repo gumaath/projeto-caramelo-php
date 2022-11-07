eebe23d6ed6fc3aa7ea775bb596faf35a757905e
gumaath <guuhsykees@gmail.com>
2022-11-07 20:27:21 -0300

    change path link css login and change logo

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="../../src/css/bootstrap.css" rel="stylesheet">
+    <link href="./src/css/bootstrap.css" rel="stylesheet">
 </head>
 
 <body>
