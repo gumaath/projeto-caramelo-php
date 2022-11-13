9e5d264a5c19390f419007c39864fcad72660e90
gumaath <guuhsykees@gmail.com>
2022-11-12 22:34:43 -0300

    removed gitignore to show autoload vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gumaath/projeto-caramelo',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'abae4d30adda2228befd44a194284200cd040818',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gumaath/projeto-caramelo' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'abae4d30adda2228befd44a194284200cd040818',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
