ed0b6f8849d107040c7333f6546f63350998a71c
Rafael Henrique <nevesrafel@hotmail.com>
2022-12-03 18:31:00 -0300

    feat: adicao demais realtorios

--- Medicines.php
+++ Medicines.php
@@ -1,13 +1,26 @@
 <?php
 include($_SERVER['DOCUMENT_ROOT'] . '/projeto-caramelo-php/vendor/autoload.php');
 
+use App\Functions;
+use App\Connect;
 use App\Auth;
 
+$db = new Connect();
+$dbcon = $db->ConnectDB();
+$_functions = new Functions($dbcon);
+
 Auth::verificaSessionLogin();
+
+$user = $_functions ::getUser();
+$pets = $_functions::getAllPetsOwner($user);
+
+if($_REQUEST && @$_POST['Pesquisar']) {
+    $data = Functions::returnMedicineData(@$_POST['id_pet'], @$_POST['med'], @$_POST['date']);
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,7 +29,6 @@ Auth::verificaSessionLogin();
     <link href="src/css/bootstrap-theme.css" rel="stylesheet" />
     <title>Medicamentos</title>
 </head>
-
 <body>
     <nav class="navbar navbar-dark bg-dark">
         <img class="mx-auto d-block img-fluid" src="src/assets/logominha.png" width="140" height="70" alt="Logo do aplicativo: AnamnePet">
@@ -24,70 +36,91 @@ Auth::verificaSessionLogin();
             <span class="navbar-toggler-icon"></span>
         </button>
     </nav>
-    <div class="collapse" id="navbarMenuOptions">
+    <div class="collapse text-center" id="navbarMenuOptions">
         <!--Opções de navegação do Menu de Navegação-->
         <div class="bg-dark p-4">
             <ul class="navbar-nav text-white">
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">Menu principal</a>
+                    <a class="nav-link" aria-current="page" href="./Main.php">Menu principal</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" aria-current="page" href="#">Meu perfil</a>
                 </li>
                 <li class="nav-item">
-                    <button class="btn btn-danger" onclick="logoutUser()">Sair</button>
+                    <button class="btn btn-danger" style="width: 60%;" onclick="logoutUser()">Sair</button>
                 </li>
             </ul>
         </div>
     </div>
     <!--Opções de navegação do Menu de Navegação-->
-    <div class="container mt-4 " style="width: 80%;">
-        <!--Container filtros-->
-        <span class="text-muted">(Obrigatório)</span>
-        <div class="input-group mb-3">
+    <form id="mainForm" method="post">
+        <div class="container mt-4 " style="width: 80%;">
+            <!--Container filtros-->
+            <span class="text-muted">(Obrigatório)</span>
+            <div class="input-group mb-3">
+                <!--Dropdown-->
+                <label class="input-group-text" for="inputGroupSelect01">Selecione um Pet</label>
+                <select class="form-select" id="inputGroupSelect01" name="id_pet" required>
+                    <option selected value="">Selecione...</option>
+                    <?php 
+                    foreach($pets as $_pet) { ?>
+                        <option value="<?= $_pet['id_pet'] ?>" required><?= $_pet['name_pet']?></option>
+                    <?php } ?>
+                </select>
+            </div>
             <!--Dropdown-->
-            <label class="input-group-text" for="inputGroupSelect01">Selecione um Pet</label>
-            <select class="form-select" id="inputGroupSelect01">
-                <option selected>Selecione...</option>
-                <option value="1">Chulé</option>
-                <option value="2">Pretinho</option>
-                <option value="3">Gordinho</option>
-            </select>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="inputGroup-sizing-default">Nome do medicamento</span>
+                <input type="text" class="form-control" name="med">
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="inputGroup-sizing-default">Data de aplicação</span>
+                <input type="date" class="form-control" name="date">
+            </div>
         </div>
-        <!--Dropdown-->
-        <div class="input-group mb-3">
-            <span class="input-group-text" id="inputGroup-sizing-default">Nome da vacina</span>
-            <input type="text" class="form-control">
+        <!--Container filtros-->
+        <div class="container d-flex justify-content-end mt-2" style="width: 80%;">
+            <!--Container que segura os buttons-->
+            <input type="submit" name="Pesquisar" class="btn btn-primary mx-2" value="Pesquisar"></input>
+            <input type="submit" class="btn btn-outline-secondary" name="Clear" value="Limpar filtros" onclick="clearFields()"></input>
         </div>
-        <div class="input-group mb-3">
-            <span class="input-group-text" id="inputGroup-sizing-default">Data de aplicação</span>
-            <input type="date" class="form-control">
-        </div>
-    </div>
-    <!--Container filtros-->
-    <div class="container d-flex justify-content-end mt-2" style="width: 80%;">
-        <!--Container que segura os buttons-->
-        <button class="btn btn-primary mx-2">Pesquisar</button>
-        <button class="btn btn-outline-secondary">Limpar filtros</button>
-    </div>
+    </form>
     <!--Container que contem os buttons-->
-    <div class="result container my-4" style="width: 80%;">
-        <h2>Resultados:</h2>
-        <hr>
+    
+    <?php if(isset($data) && $data != null) { ?>
+        <div class="result container my-4" style="width: 80%;">        
+            <h2>Resultados:</h2>
+            <hr>
+        </div>
+    <?php
+        foreach($data as $_data)  {
+    ?>
+    <div class="result container my-4" style="width: 80%;">        
         <div class="card">
-            <div class="card-header">V3</div>
+            <div class="card-header"><?= $_data['name_med']?></div>
             <div class="card-body">
-                <p class="card-text">Pet: Branquinho</p>
-                <p class="card-text">Previne: Panleucopenia, Rinotraqueíte e outras doenças</p>
+                <p class="card-text">Pet:  <?= $_data['pet'] ?></p>
+                <p class="card-text">Orientação: <?= $_data['descricao'] ?></p>
                 <span class="card-text">Data de aplicação:</span>
-                <span class="text-muted d-inline">06/11/2022</span>
-                <button data-bs-toggle="collapse" class="btn btn-primary d-block mt-3" data-bs-target="#attachment">Ver anexo</button>
-                <div id="attachment" class="collapse mt-2 border">
-                    <img class="zoom" src="src/assets/logominha.png" alt="..." width="100%" height="120">
-                </div>
+                <span class="text-muted d-inline"><?= Functions::formatDate($_data['date_med']) ?></span>
+                <p class="mt-3">Veterinário responsável: <?= $_data['vet'] ?></p>               
+            </div>
+            <div class="card-footer">
+                <a class="mx-2 contact-btn" href="tel:<?= $_data['fone'] ?>">
+                    <img src="src/assets/phone-call.png" title="Ligar para o veterinário responsável" width="36" class="img-fluid" alt="phone-icon">
+                </a>          
+                <a target="_blank" id="lnk_wpp" class="mx-2 contact-btn" href="https://api.whatsapp.com/send?phone=55<?=Functions::formatWhatsApp($_data['fone'])?>">
+                    <img src="src/assets/whatsapp.png" title="Abrir conversa no Whatsapp" width="38" class="img-fluid" alt="zapzap-icon">
+                </a> 
             </div>
         </div>
     </div>
+    <?php } } ?>
+    <?php if(@$data == null && $_REQUEST && !@$_POST['Clear']) { ?>
+        <div class="text-center container-fluid mt-5">
+            <h4 class="mt-5">Não existem medicamentos aplicados com os parâmetros selecionados!</h4>
+        </div>
+    <?php } ?>
     <script src="https://cdn.jsdelivr.net/npm/medium-zoom@1.0.6/dist/medium-zoom.min.js"></script>
     <script src="./src/js/bootstrap.bundle.min.js"></script>
     <script src="./src/js/scripts.js"></script>
@@ -95,6 +128,12 @@ Auth::verificaSessionLogin();
         mediumZoom('.zoom', {
             margin: 50
         })
+
+        function clearFields() {
+            var form = document.getElementById("mainForm");
+            form.reset();
+        }        
+
     </script>
 </body>
 
